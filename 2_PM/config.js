8104f3d706113b31d8a892c5aa1ed9a863b3d4a3
Rich Jenkins <richardjenkins@college.harvard.edu>
2019-01-08 16:46:20 -0500

    pm work

--- 2_PM/config.js
+++ 2_PM/config.js
@@ -1,3 +1,9 @@
 // TODO: Add Google API key here - first hardcoded, then as environment variable.
+const dotenv = require('dotenv')
+dotenv.config()
 
-const config = {};
+console.log(process.env.ENV_VARIABLE)
+
+const config = {API_KEY: 'AIzaSyBXGSsSDuDecaYMwfAbE54T0VzM5Sm6cbo'};
+
+module.exports = config 
--- 2_PM/placeinfo.js
+++ 2_PM/placeinfo.js
@@ -2,3 +2,12 @@
 / returns the following string, formatted with the appropriate values:
 
 'INPUTTED_PLACE is located at coordinates RETURNED_COORDINATES, at the address RETURNED_ADDRESS.' */
+const googleMapsClient = require('./google_api.js')
+
+googleMapsClient.findPlace({input: process.argv[2], inputtype: 'textquery'}).asPromise()
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
